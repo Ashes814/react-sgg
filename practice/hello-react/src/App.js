737f0cc042b57295d3027fb622b7ebffe2ae3e12
张欧文 <1534661514@qq.com>
2022-12-24 19:34:42 +0800

    hello react staging

--- practice/hello-react/src/App.js
+++ practice/hello-react/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import react core
+import React, { Component } from "react";
+import Hello from "./Components/Hello";
+import Welcome from "./Components/Welcome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// create skin component App and exposure
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Welcome />
+      </div>
+    );
+  }
 }
-
-export default App;
