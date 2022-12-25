78912b29f7fa448b916b877bff82eedda54e943e
张欧文 <1534661514@qq.com>
2022-12-25 18:52:18 +0800

    add todo list

--- practice/hello-react/src/App.jsx
+++ practice/hello-react/src/App.jsx
@@ -5,12 +5,33 @@ import Footer from "./Components/Footer";
 import "./App.css";
 
 export default class App extends Component {
+  state = {
+    todos: [
+      { id: "001", name: "eat", done: true },
+      { id: "002", name: "sleep", done: true },
+      { id: "003", name: "coding", done: false },
+      { id: "004", name: "shopping", done: true },
+    ],
+  };
+
+  // addTodo is for add a todo, which accept a todo object as parameter
+  addTodo = (todoObj) => {
+    const { todos } = this.state;
+
+    const newTodos = [todoObj, ...todos];
+    this.setState({ todos: newTodos });
+  };
+
+  itemtest = () => {
+    console.log("itemtest", this);
+  };
   render() {
+    const { todos } = this.state;
     return (
       <div className="todo-container">
         <div className="todo-wrap">
-          <Header />
-          <List />
+          <Header addTodo={this.addTodo} />
+          <List todos={todos} itemtest={this.itemtest} />
           <Footer />
         </div>
       </div>
--- practice/hello-react/src/Components/List/index.jsx
+++ practice/hello-react/src/Components/List/index.jsx
@@ -4,9 +4,13 @@ import "./index.css";
 
 export default class List extends Component {
   render() {
+    const { todos } = this.props;
+    const { itemtest } = this.props;
     return (
       <ul className="todo-main">
-        <Item />
+        {todos.map((todo) => {
+          return <Item key={todo.id} {...todo} />;
+        })}
       </ul>
     );
   }
