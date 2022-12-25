fb31d17d04cf0bded933811ddf2cd53866b9ec62
张欧文 <1534661514@qq.com>
2022-12-25 15:20:26 +0800

    implement static components

---
+++ practice/hello-react/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Header from "./Components/Header";
+import List from "./Components/List";
+import Footer from "./Components/Footer";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List />
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
