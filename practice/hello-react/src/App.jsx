8ae06a407f00c3202321a13d68fa434d42fcb91b
张欧文 <1534661514@qq.com>
2022-12-30 15:02:31 +0800

    update getch ajax

--- practice/hello-react/src/App.jsx
+++ practice/hello-react/src/App.jsx
@@ -3,22 +3,11 @@ import Search from "./Components/Search";
 import List from "./Components/List";
 
 export default class App extends Component {
-  state = {
-    users: [], //初始化状态，users初始值为数组
-    isFirst: true, // 是否为第一次打开页面
-    isLoading: false, // 标识是否处于加载中
-    err: "", //存储请求相关的错误信息
-  };
-
-  updateAppState = (stateObj) => {
-    this.setState(stateObj);
-  };
-
   render() {
     return (
       <div className="container">
-        <Search updateAppState={this.updateAppState} />
-        <List {...this.state} />
+        <Search />
+        <List />
       </div>
     );
   }
--- practice/hello-react/src/Components/Search/index.jsx
+++ practice/hello-react/src/Components/Search/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PubSub from "pubsub-js";
 import axios from "axios";
 
 export default class Search extends Component {
@@ -7,23 +8,26 @@ export default class Search extends Component {
     const {
       keyWordElement: { value: keyword },
     } = this;
-    //发送请求前通知App更新状态
-    this.props.updateAppState({ isFirst: false, isLoading: true });
 
+    //发送请求前通知List更新状态
+    PubSub.publish("tieite", { isFirst: false, isLoading: true });
     // 发送网络请求
     axios.get(`http://localhost:3000/api1/search/users?q=${keyword}`).then(
       (response) => {
-        //请求成功后通知app更新状态
-
-        this.props.updateAppState({
+        //请求成功后通知List更新状态
+        PubSub.publish("tieite", {
           err: "",
           isLoading: false,
           users: response.data.items,
         });
+        //     this.props.updateAppState({
+        //       err: "",
+        //       isLoading: false,
+        //       users: response.data.items,
       },
       (error) => {
-        //请求失败后通知app更新状态
-        this.props.updateAppState({
+        //请求失败后通知List更新状态
+        PubSub.publish("tieite", {
           isLoading: false,
           err: error.message,
         });
