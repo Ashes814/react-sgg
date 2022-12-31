f69222778d559b965707897faa8119384ea3b43b
张欧文 <1534661514@qq.com>
2022-12-31 13:56:49 +0800

    init react router

--- practice/hello-react/src/App.jsx
+++ practice/hello-react/src/App.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import Search from "./Components/Search";
-import List from "./Components/List";
 
 export default class App extends Component {
   render() {
-    return (
-      <div className="container">
-        <Search />
-        <List />
-      </div>
-    );
+    return <div className="container"></div>;
   }
 }
