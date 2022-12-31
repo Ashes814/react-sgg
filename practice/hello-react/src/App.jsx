2172e72dfba4c43762d0299b071601b33e4b6a69
张欧文 <1534661514@qq.com>
2022-12-31 14:57:32 +0800

    browser router and link

--- practice/hello-react/src/App.jsx
+++ practice/hello-react/src/App.jsx
@@ -1,7 +1,52 @@
 import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
 
 export default class App extends Component {
   render() {
-    return <div className="container"></div>;
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <h2>React Router Demo</h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中， 靠a跳转不同的页面 */}
+              {/* <a className="list-group-item" href="./about.html">
+                About
+              </a>
+              <a className="list-group-item active" href="./home.html">
+                Home
+              </a> */}
+              {/* react中， 靠路由链接切换组件 */}
+
+              <Link className="list-group-item" to="/about">
+                About
+              </Link>
+              <Link className="list-group-item" to="/home">
+                Home
+              </Link>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由（编写路由链接） */}
+
+                <Route path="/about" component={About}></Route>
+                <Route path="/home" component={Home}></Route>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
