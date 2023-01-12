3dacb0b591a6a62a6f0f2f453fa31f64387dddf7
张欧文 <1534661514@qq.com>
2023-01-12 20:53:51 +0800

    antd style modified

--- practice/hello-react/src/App.jsx
+++ practice/hello-react/src/App.jsx
@@ -2,33 +2,39 @@ import React, { Component } from "react";
 import { Button, Space } from "antd";
 
 import { GithubOutlined } from "@ant-design/icons";
+import { ConfigProvider } from "antd";
+// import { red } from "@ant-design/colors";
 import type { DatePickerProps } from "antd";
 import { DatePicker } from "antd";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Header from "./components/Header";
-import MyNavLink from "./components/MyNavLink";
+
 // import Test from "./components/Test";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Button type="primary">click me</Button>
-        <Button type="link">click me2</Button>
-        <Button>click me3</Button>
-        <Space>
-          <GithubOutlined />
-        </Space>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: "#00b96b",
+            },
+          }}
+        >
+          <Button type="primary">click me</Button>
+          <Button type="link">click me2</Button>
+          <Button>click me3</Button>
+          <Space>
+            <GithubOutlined />
+          </Space>
 
-        <Space direction="vertical">
-          <DatePicker />
-          <DatePicker picker="week" />
-          <DatePicker picker="month" />
-          <DatePicker picker="quarter" />
-          <DatePicker picker="year" />
-        </Space>
+          <Space direction="vertical">
+            <DatePicker />
+            <DatePicker picker="week" />
+            <DatePicker picker="month" />
+            <DatePicker picker="quarter" />
+            <DatePicker picker="year" />
+          </Space>
+        </ConfigProvider>
       </div>
     );
   }
