eab703df795b9af1037a492b55d4637dbdf0befe
张欧文 <1534661514@qq.com>
2023-01-12 20:04:28 +0800

    antd basic

--- practice/hello-react/src/App.jsx
+++ practice/hello-react/src/App.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { Button, Space } from "antd";
+
+import { GithubOutlined } from "@ant-design/icons";
+import type { DatePickerProps } from "antd";
+import { DatePicker } from "antd";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,43 +15,20 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-            <div className="page-header">
-              <Header />
-            </div>
-          </div>
-        </div>
+        <Button type="primary">click me</Button>
+        <Button type="link">click me2</Button>
+        <Button>click me3</Button>
+        <Space>
+          <GithubOutlined />
+        </Space>
 
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              {/* 原生html中， 靠a跳转不同的页面 */}
-              {/* <a className="list-group-item" href="./about.html">
-                About
-              </a>
-              <a className="list-group-item active" href="./home.html">
-                Home
-              </a> */}
-              {/* react中， 靠路由链接切换组件 */}
-
-              <MyNavLink to="/about">About</MyNavLink>
-              <MyNavLink to="/home">Home</MyNavLink>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                {/* 注册路由（编写路由链接） */}
-                <Switch>
-                  <Route path="/about" component={About}></Route>
-                  <Route path="/home" component={Home}></Route>
-                  <Redirect to="/about" />
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Space direction="vertical">
+          <DatePicker />
+          <DatePicker picker="week" />
+          <DatePicker picker="month" />
+          <DatePicker picker="quarter" />
+          <DatePicker picker="year" />
+        </Space>
       </div>
     );
   }
