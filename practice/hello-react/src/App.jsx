f0d806f74e6a583ab2ec1e0a110fca399eacaec0
张欧文 <1534661514@qq.com>
2023-01-17 19:14:33 +0800

    init react6

--- practice/hello-react/src/App.jsx
+++ practice/hello-react/src/App.jsx
@@ -1,12 +1,5 @@
-import React, { Component, Fragment } from "react";
-import Demo from "./components/8_errorBoundary/Parent";
+import React from "react";
 
-export default class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Demo />
-      </Fragment>
-    );
-  }
+export default function App() {
+  return <div>App</div>;
 }
