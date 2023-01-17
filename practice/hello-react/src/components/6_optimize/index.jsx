6fee5f52d74ec8109a6d07934a0ba310104372c3
张欧文 <1534661514@qq.com>
2023-01-17 12:38:47 +0800

    implement pure component

--- practice/hello-react/src/components/6_optimize/index.jsx
+++ practice/hello-react/src/components/6_optimize/index.jsx
@@ -1,14 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 import "./index.css";
 
-export default class Parent extends Component {
+export default class Parent extends PureComponent {
   state = { carName: "c63 AMG" };
 
   changeCar = () => {
-    this.setState({ carName: "Laferrari" });
+    this.setState({ carName: "!!!" });
   };
 
+  //   shouldComponentUpdate(nextProps, nextState) {
+  //     return true;
+  //   }
+
   render() {
+    console.log("parent");
     return (
       <div className="parent">
         <h3>I am Parent, I have a {this.state.carName}</h3>
@@ -18,12 +23,19 @@ export default class Parent extends Component {
     );
   }
 }
-class Child extends Component {
+class Child extends PureComponent {
+  //   shouldComponentUpdate(nextProps, nextState) {
+  //     console.log(this.props, this.state);
+  //     console.log(nextProps, nextState);
+  //     return this.props.carName !== nextProps.carName;
+  //   }
+
   render() {
+    console.log("child");
     return (
       <div className="child">
         <h3>I am Child</h3>
-        <h4>My Father has a {this.props.carName}</h4>
+        <h4>My Father has a {"?"}</h4>
       </div>
     );
   }
